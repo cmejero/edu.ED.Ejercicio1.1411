43d0da7410b812741a62399e6cd3a6ecfee1ffb2
csi23-cmejero <csi23-cmejero@colegioaltair.net>
2023-11-21 12:33:05 +0100

    no message

---
+++ edu.ED.Ejercicio11411/src/Controladores/Inicio.java
@@ -0,0 +1,21 @@
+package Controladores;
+
+public class Inicio {
+
+	public static void main(String[] args) 
+    {
+		
+		
+		int aux = 10; 
+		System.out.println(aux);
+       for( int i= 0; i<=20 ; i++) 
+       {
+    	   
+	     aux= aux * 2;
+	     System.out.println(aux);
+       }
+      
+       
+	}
+
+}
