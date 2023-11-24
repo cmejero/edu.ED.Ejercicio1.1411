0e0ce8929f1222f3a7a513724380d6b104800daa
csi23-cmejero <csi23-cmejero@colegioaltair.net>
2023-11-24 08:36:47 +0100

    no message

--- edu.ED.Ejercicio11411/src/Controladores/Inicio.java
+++ edu.ED.Ejercicio11411/src/Controladores/Inicio.java
@@ -1,21 +1,26 @@
 package Controladores;
-
+import java.util.Scanner;
 public class Inicio {
 
 	public static void main(String[] args) 
     {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Numero de eses pago: ");
+		int numMeses = sc.nextInt();
 		
 		int aux = 10; 
 		System.out.println(aux);
-       for( int i= 0; i<=20 ; i++) 
+       for( int i= 220; i<=numMeses ; i++) 
        {
     	   
 	     aux= aux * 2;
 	     System.out.println(aux);
        }
+      System.out.println("-----");
+      System.out.println(aux);
       
-       
+       //sc.next(); --> para string.
+       //sc.next().charAt(0); --> para caracteres.
 	}
 
 }
